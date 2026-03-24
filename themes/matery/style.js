133ae1d7c5fbc5839a9572d83090171bcd80beaf
mogeko020715-wq <mogeko020715@gmail.com>
2026-03-25 00:17:58 +0800

    Update style.js

--- themes/matery/style.js
+++ themes/matery/style.js
@@ -44,12 +44,12 @@ const Style = () => {
       }
 
       ::-webkit-scrollbar-thumb {
-        background-color: #4338ca;
+        background-color: #adb39a;
       }
 
       * {
         scrollbar-width: thin;
-        scrollbar-color: #4338ca transparent;
+        scrollbar-color: #adb39a transparent;
       }
     `}</style>
   )
